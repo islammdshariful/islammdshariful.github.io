3c7ede6e3f8ebbd09ed227b034ad4b3cb6d5c403
islammdshariful <islammdshariful@users.noreply.github.com>
2026-09-25 18:42:11 +0000

    deploy: update site islammdshariful/islammdshariful-blog@1b39656e54f35c0e7f9e9d26be5f5e7b7b7a494e

---
+++ _astro/PostLayout.astro_astro_type_script_index_0_lang.Boksc85P.js
@@ -0,0 +1,1 @@
+import{m as a}from"./food-client.CiiDEYkV.js";document.addEventListener("astro:page-load",()=>{document.body.classList.contains("layout-post")&&a(location.pathname.replace(/^\/+|\/+$/g,""))});
